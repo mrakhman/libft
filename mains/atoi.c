1a0306cd8b40bee95b0975cd19b7c662f1879c02
Mariya RAKHMAN <mrakhman@student.42.fr>
2017-11-28 21:06:11 +0100

    save mains

--- mains/atoi.c
+++ mains/atoi.c
@@ -6,7 +6,7 @@
 /*   By: mrakhman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 15:44:30 by mrakhman          #+#    #+#             */
-/*   Updated: 2017/11/28 15:58:01 by mrakhman         ###   ########.fr       */
+/*   Updated: 2017/11/28 20:51:15 by mrakhman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 int	main(void)
 {
-	char str[] = "\n\t-9097";
-	printf("%d\n", ft_atoi(str));
-	printf("%d", atoi(str));
+	char	str1[] = "\n\t-9097";
+	char	str2[] = "hello1";
+	printf("%d\n", ft_atoi(str1));
+	printf("%d\n\n", atoi(str1));
+	printf("%d\n", ft_atoi(str2));
+	printf("%d\n", atoi(str2));
 	return (0);
 }
