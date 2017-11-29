489e88397fc32272b6f3cf3758d5356388925d84
Mariya RAKHMAN <mrakhman@student.42.fr>
2017-11-29 18:22:46 +0100

    save mains

--- mains/isalpha.c
+++ mains/isalpha.c
@@ -6,7 +6,7 @@
 /*   By: mrakhman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 19:07:57 by mrakhman          #+#    #+#             */
-/*   Updated: 2017/11/28 19:21:21 by mrakhman         ###   ########.fr       */
+/*   Updated: 2017/11/29 18:20:38 by mrakhman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 
 int	main(void)
 {
-	char	*str1;
-	char	*str2;
+	char	a;
+	char	b;
+	char	c;
 
-	str1 = "Hello is it number 10?";
-	str2 = "IcantThinkOfGettingOld";
-	printf("%d, %d\n", ft_isalpha(str1), ft_isalpha(str2));
-	printf("%d, %d\n", isalpha(str1), isalpha(str2));
+	a = 'a';
+	b = 'O';
+	c = '9';
+	printf("%d, %d, %d\n", ft_isalpha(a), ft_isalpha(b), ft_isalpha(c));
+	printf("%d, %d, %d\n", isalpha(a), isalpha(b), isalpha(c));
 	return (0);
 }
