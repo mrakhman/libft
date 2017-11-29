efc7a22367511b859cfd0378711aff9c0411c3fe
Mariya RAKHMAN <mrakhman@student.42.fr>
2017-11-29 18:50:41 +0100

    save strdup

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrakhman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/29 18:24:04 by mrakhman          #+#    #+#             */
+/*   Updated: 2017/11/29 18:49:49 by mrakhman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strdup(char *src)
+{
+	int		i;
+	char	*copy;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	copy = (char*)malloc(sizeof(*src) * (i + 1));
+	if (copy == NULL)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
